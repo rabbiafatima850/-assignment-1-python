e59cdea2cd9ea8f418ee4f1e16dfcff4b78d0b78
rabbiafatima850 <156567170+rabbiafatima850@users.noreply.github.com>
2024-10-24 10:23:00 -0700

    Add files via upload

---
+++ py assign 1.py
@@ -0,0 +1,47 @@
+# Q1:
+
+num1 = int(input('Enter your first number : '))
+num2 = int(input('Enter your second number : '))
+num3= int(input('Enter your third number : '))
+
+sum = num1+num2+num3
+print(sum)
+
+difference= num1-num2-num3
+print(difference)
+
+product= num1*num2*num3
+print(product)
+
+average= sum/3
+print(average)
+
+# Q2:
+#(1)  Area1=21*base*height
+
+a=int(input('enter your base: '))
+b=int(input('enter your height: '))
+area1= 21*a*b
+print(area1)
+
+#(2)  Area2 =side**2
+
+c=int(input('enter your side: '))
+area2 =c**2
+print(area2)
+
+#(3)  area3 = 3.14*radius**2
+
+d=int(input('enter your radius:'))
+area3 =3.14*d**2
+print(area3)
+
+#(4)  area4= length*width
+
+e=int(input('enter your length: '))
+f=int(input('enter your width: '))
+area4= e*f 
+print(area4)
+
+
+
